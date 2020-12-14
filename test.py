0c7e341821c9b0cb20ea885ccba9896615c798b6
Jon Kent <jonkent95@gmail.com>
2020-12-14 14:49:30 +0000

    unique tx ids

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import json
+import uuid
+
 from app.tester import run_test
+
+
 def run_tests():
     payload = '''{
     "collection": {
@@ -31,19 +36,17 @@ def run_tests():
     "type": "uk.gov.ons.edc.eq:surveyresponse",
     "version": "0.0.1",
     "survey_id": "009",
-    "case_id": "4c0bc9ec-06d4-4f66-88b6-2e42b79f17b3",
-    "tx_id": "b93d5852-359d-4e37-8338-c949bcb98617"
+    "case_id": "4c0bc9ec-06d4-4f66-88b6-2e42b79f17b3"
 }'''
-    tx_id = "b93d5852-359d-4e37-8338-c949bcb98617"
-    # with open("./keys.yml") as file:
-    #     secrets_from_file = yaml.safe_load(file)
-    #
-    # key_store = KeyStore(secrets_from_file)
-    # payload = encrypt(submission, key_store, 'submission')
-    #
+
+    submission = json.loads(payload)
+    tx_id = str(uuid.uuid4())
+    submission['tx_id'] = tx_id
+    payload = json.dumps(submission)
     passed = run_test(payload, tx_id)
     if passed:
         return True
+
 
 if __name__ == '__main__':
     run_tests()
