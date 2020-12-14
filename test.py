e78438b7711f6749de3a92cc4499d9ed1dfb08f3
bkhan2 <mohammed.khan@ons.gov.uk>
2020-12-14 10:34:53 +0000

    changes to pipeline tests

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
-def run_tests():
-    print("running tests in python!!!")
-    return True
+from app.tester import run_test
+# def run_tests():
+#     print("running tests in python!!!")
+#     return True
 
 
 if __name__ == '__main__':
-    run_tests()
+    run_test()
