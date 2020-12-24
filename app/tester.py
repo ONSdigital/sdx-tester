1a3d2910f656cdcf6528f316f943dfd17a6a4293
TomHolroyd <tomholroyd96@gmail.com>
2020-12-24 11:11:55 +0000

    Removing decryption method call

--- app/tester.py
+++ app/tester.py
@@ -1,5 +1,5 @@
 from app import store_reader
-from app.encryption import encrypt_survey, decrypt_zip, view_zip_content
+from app.encryption import encrypt_survey, view_zip_content
 from app.publish import publish_data
 from app.subscriber import SurveyListener
 
@@ -13,9 +13,8 @@ def run_test(data: dict, tx_id: str) -> bool:
         # file_name = store_reader.get_filename(message_str)
         file_data = store_reader.read(tx_id)
         print(f"validating response...")
-        print(file_data)
-        file_data_decrypted = decrypt_zip(file_data)
-        view_zip_content(file_data_decrypted)
+        # file_data_decrypted = decrypt_zip(file_data)
+        view_zip_content(file_data)
         return True
 
     test_subscriber = SurveyListener(tx_id, validate)
