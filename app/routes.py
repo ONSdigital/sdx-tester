c96997e440559b4784518cd824d46e28cc626e1f
TomHolroyd <tomholroyd96@gmail.com>
2021-01-05 15:19:39 +0000

    Route added for tx_id page and tx_id has working href

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+import json
 import logging
 import uuid
-from flask import request, render_template, jsonify
+from datetime import datetime
+
+from flask import request, render_template, url_for
 from structlog import wrap_logger
+from werkzeug.utils import redirect
 
 from app import app
 from app.messaging import message_manager
@@ -9,15 +13,14 @@ from app.tester import run_test
 from app.read_data import extract_test_data
 
 logger = wrap_logger(logging.getLogger(__name__))
-tx_list = ["40e659ec-013f-4993-9a31-ec1e0ad37888", "9db49fb8-4bcc-4615-b42c-b2ab80b234b8",
-           "d6d7acf4-52f4-41f6-83b1-c0806904ca42", "308409e4-cf09-46cb-8988-f85e7116ade6",
-           "b7f78b30-1814-45ac-963d-8c997c091f90"]
+tx_list = {}
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     surveys = extract_test_data()
+    print(tx_list)
     return render_template('index.html',
                            surveys=surveys,
                            tx_list=tx_list)
@@ -25,15 +28,19 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    surveys = extract_test_data()
     data_str = request.form.get('post-data')
-    data_dict = eval(data_str)
+    data_dict = json.loads(data_str)
     tx_id = str(uuid.uuid4())
     data_dict['tx_id'] = tx_id
-    tx_list.append(tx_id)
+    time = datetime.now()
+    time = time.strftime("%H:%M:%S")
+    tx_list[time] = tx_id
     result = run_test(message_manager, data_dict)
     passed = result.dap_message is not None
-    print(passed)
-    return render_template('index.html',
-                           surveys=surveys,
-                           tx_list=tx_id)
+    return redirect(url_for('index'))
+
+
+@app.route('/response/<tx_id>', methods=['GET'])
+def view_response(tx_id):
+    return render_template('response.html',
+                           tx_id=tx_id)
