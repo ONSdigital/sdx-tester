05070bde485ab10ef6acaf4d57bea505eecc5e87
TomHolroyd <tomholroyd96@gmail.com>
2021-01-15 13:19:27 +0000

    Listing all pods

--- test_cron.py
+++ test_cron.py
@@ -1,6 +1,16 @@
-import os
-
-os.system("kubectl create job --from=cronjob/sdx-collate test-collate")
+# import os
+#
+# os.system("kubectl create job --from=cronjob/sdx-collate test-collate3")
 
 # Function to check bucket for comments and validate
 # def check_bucket()
+from kubernetes import client, config
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
