c03abc6a55f6e7b2d6057d074e52d33e9d23a279
TomHolroyd <tomholroyd96@gmail.com>
2021-01-15 13:37:59 +0000

    Testing kubectl api

--- test_cron.py
+++ test_cron.py
@@ -1,16 +1,31 @@
+from kubernetes import client, config
+
 # import os
 #
 # os.system("kubectl create job --from=cronjob/sdx-collate test-collate3")
 
 # Function to check bucket for comments and validate
 # def check_bucket()
-from kubernetes import client, config
+job_name = 'test'
+cron_job_name = 'sdx-collate'
+namespace = 'default'
 
-# Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
+batch_v1 = client.BatchV1Api()
+batch_v1beta1 = client.BatchV1beta1Api()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+cron_job = batch_v1beta1.read_namespaced_cron_job(cron_job_name, namespace)
+
+job = client.V1Job(
+    api_version='batch/v1',
+    kind='Job',
+    metadata=client.models.V1ObjectMeta(
+        name=job_name,
+        # This annotation is added by kubectl, probably best to add it ourselves as well
+        annotations={"cronjob.kubernetes.io/instantiate": "manual"}
+    ),
+    spec=cron_job.spec.job_template.spec
+)
+
+result = batch_v1.create_namespaced_job(namespace, job)
+print(result)
