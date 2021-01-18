909154ce02a41db701fb92f0daabc574d2fcd158
TomHolroyd <tomholroyd96@gmail.com>
2021-01-18 14:09:11 +0000

    validate cron now validates zip file

--- validate_cron.py
+++ validate_cron.py
@@ -1,15 +1,18 @@
-import time
+from google.api_core.exceptions import NotFound
 from datetime import datetime
 
-from app.store_reader import read
+from app.store_reader import read, extract_zip
 
 
 def check_bucket_for_comments():
-    time.sleep(10)
     date_time = datetime.utcnow()
-    file_name = f"{date_time.strftime('%Y-%m-%d')}.zip"
-    comments = read(file_name, 'comments')
-    if comments:
-        return True
-    else:
-        return False
+    file_name = f"{date_time.strftime('%Y-%m-%d')}.zi"
+    try:
+        extract_zip(read(file_name, 'comments'))
+    except NotFound as err:
+        print(err)
+        return err
+
+
+if __name__ == '__main__':
+    check_bucket_for_comments()
