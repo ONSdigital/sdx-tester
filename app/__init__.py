ec035412de97a386877d6ad39c2faa19b690b440
bkhan2 <mohammed.khan@ons.gov.uk>
2021-02-24 13:59:09 +0000

    adding updated config class in main init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import gnupg
 
+from google.cloud import storage
 from flask import Flask
 from flask_socketio import SocketIO
 
@@ -13,7 +15,41 @@ logging.basicConfig(
     level=LOGGING_LEVEL,
 )
 
-PROJECT_ID = os.getenv('PROJECT_ID', 'ons-sdx-sandbox')
+project_id = os.getenv('PROJECT_ID', 'ons-sdx-sandbox')
+
+class Config:
+    def __init__(self, proj_id):
+        self.PROJECT_ID = proj_id
+        self.BUCKET_NAME = f'{proj_id}-outputs'
+        self.SEFT_BUCKET_NAME = f'{proj_id}-sefts'
+        self.BUCKET = None
+        self.SEFT_BUCKET = None
+        self.SURVEY_TOPIC_PATH = None
+        self.SEFT_TOPIC_PATH = None
+        self.DAP_SUBSCRIPTION = None
+        self.QUARANTINE_SUBSCRIPTION = None
+        self.SEFT_QUARANTINE_SUBSCRIPTION = None
+        self.RECEIPT_SUBSCRIPTION = None
+        self.GPG = None
+        self.ENCRYPTION_KEY = None
+        self.KEY_PURPOSE_SUBMISSION = None
+
+CONFIG = Config(project_id)
+
+def cloud_config():
+    print('loading cloud config')
+    CONFIG.DAP_SUBSCRIPTION = "dap-subscription"
+    gpg = gnupg.GPG()
+    with open('dap_private_key.asc') as f:
+        key_data = f.read()
+        f.close()
+    gpg.import_keys(key_data)
+    CONFIG.ENCRYPTION_KEY = key_data
+    CONFIG.GPG = gpg
+    storage_client = storage.Client(CONFIG.PROJECT_ID)
+    CONFIG.BUCKET = storage_client(CONFIG.BUCKET_NAME)
+    CONFIG.SEFT_BUCKET = storage_client(CONFIG.SEFT_BUCKET_NAME)
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
