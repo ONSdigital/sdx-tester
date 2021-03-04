b18f1b7f901e635d0f84786fdd81f537727a2424
Chaitali15 <chaitu.tanawade@gmail.com>
2021-03-04 10:12:59 +0000

    remove receipt button in response page

--- app/survey_loader.py
+++ app/survey_loader.py
@@ -3,21 +3,21 @@ import json
 
 
 # This method produces a dict of surveys with the survey_id as the key. This is used for the UI element of SDX-tester
-def read_data(filter_func) -> dict:
-    dict_of_test_surveys = {}
+def read_survey(filter_func) -> dict:
     sorted_dict = {}
-    path1 = 'app/Data/surveys'
-    path2 = 'app/Data/sefts'
-    for filename in os.listdir(path1):
+    dict_of_test_surveys = {}
+    survey_path = 'app/Data/surveys'
+    seft_path = 'app/Data/sefts'
+    for filename in os.listdir(survey_path):
         if filter_func(filename):
-            with open(f'{path1}/{filename}', 'r') as data:
+            with open(f'{survey_path}/{filename}', 'r') as data:
                 survey = json.load(data)
                 # survey_key = f"{survey['survey_id']}-{survey['collection']['instrument_id']}"
                 survey_key = f"{survey['survey_id']}"
                 dict_of_test_surveys[survey_key] = survey
 
-    for filename in os.listdir(path2):
-        with open(f'{path2}/{filename}', 'rb') as seft_file:
+    for filename in os.listdir(seft_path):
+        with open(f'{seft_path}/{filename}', 'rb') as seft_file:
             survey_id = filename.split('.')[0]
             seft_key = f'seft_{survey_id}'
             dict_of_test_surveys[seft_key] = seft_file.read()
@@ -29,22 +29,22 @@ def read_data(filter_func) -> dict:
 
 
 def read_all() -> dict:
-    return read_data(lambda filename: True)
+    return read_survey(lambda filename: True)
 
 
 def get_dap() -> dict:
     dap_ids = ["023", "134", "147", "281", "283"]
-    return read_data(lambda filename: filename[0:3] in dap_ids)
+    return read_survey(lambda filename: filename[0:3] in dap_ids)
 
 
 def get_legacy() -> dict:
     non_legacy_ids = ["023", "134", "147", "281", "283", "lms"]
-    return read_data(lambda filename: filename[0:3] not in non_legacy_ids)
+    return read_survey(lambda filename: filename[0:3] not in non_legacy_ids)
 
 
 def get_feedback() -> dict:
-    return read_data(lambda filename: 'feedback' in filename)
+    return read_survey(lambda filename: 'feedback' in filename)
 
 
 def get_specific(survey_id: str, instrument_id: str):
-    return read_data(lambda filename: filename == f'{survey_id}.{instrument_id}.json')
+    return read_survey(lambda filename: filename == f'{survey_id}.{instrument_id}.json')
