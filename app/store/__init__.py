f04d546b6ecc95fa5024e37db0bdb63986334210
Jon Kent <jonkent95@gmail.com>
2021-04-12 17:11:04 +0100

    change seft bucket name

--- app/store/__init__.py
+++ app/store/__init__.py
@@ -2,6 +2,6 @@ from google.cloud import storage
 from app import PROJECT_ID
 
 OUTPUT_BUCKET_NAME = f'{PROJECT_ID}-outputs'
-INPUT_SEFT_BUCKET = f'{PROJECT_ID}-sefts'
+INPUT_SEFT_BUCKET = f'{PROJECT_ID}-seft-responses'
 
 storage_client = storage.Client(PROJECT_ID)
