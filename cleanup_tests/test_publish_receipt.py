1ff26b5362d69e363731685548474d49b2236f0a
TomHolroyd <tomholroyd96@gmail.com>
2021-05-17 15:30:01 +0100

    Fixing cleanup tests

--- cleanup_tests/test_publish_receipt.py
+++ cleanup_tests/test_publish_receipt.py
@@ -13,16 +13,6 @@ class TestCleanup(unittest.TestCase):
         Publishes a PuSub message for each element placed within the bucket.
         """
         test_data.pop('seft-input')
-        test = {'dataset': '009|dap|087bfc03-8698-4137-a3ac-7a596b9beb2b'}
-        test_str = json.dumps(test).encode()
         for data, filename in test_data.items():
-            print(filename.split('/', 1)[1])
-            dap_message = {
-                'body': b"Im a byte string",
-                'ordering_key': '',
-                'attributes': {
-                    "gcs.bucket": filename.split('/', 1)[0],
-                    "gcs.key": filename.split('/', 1)[1]
-                }
-            }
-            publish_dap_receipt(dap_message, 'testing_cleanup')
+            dap_message = json.dumps({'dataset': f"009|{filename.split('/', 1)[1]}"}).encode()
+            publish_dap_receipt(dap_message)
