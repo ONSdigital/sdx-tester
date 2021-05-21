59108e4020ebda3d4e1f2101ef6bd3a02cef0124
Jon Kent <jonkent95@gmail.com>
2021-05-21 15:45:05 +0100

    Recipient from config map

--- app/gpg/__init__.py
+++ app/gpg/__init__.py
@@ -1,19 +1,14 @@
 import gnupg
 
-from app import PROJECT_ID
+from app import PROJECT_ID, DATA_RECIPIENT
 from app.secret_manager import get_secret
 
 gpg = gnupg.GPG()
 encryption_key = get_secret(PROJECT_ID, 'dap-public-gpg')
 
-if PROJECT_ID == "ons-sdx-preprod":
-    recipients = ["sdx_preprod@ons.gov.uk"]
-else:
-    recipients = ['dap@ons.gov.uk']
-
 gpg.import_keys(encryption_key)
 
-RECIPIENTS = recipients
+RECIPIENTS = [DATA_RECIPIENT]
 
 with open('dap_private_gpg.asc') as f:
     key_data = f.read()
