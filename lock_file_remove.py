06cfafa4cb1ea95bc30a27cc9e8f07164919b264
TomHolroyd <tomholroyd96@gmail.com>
2021-06-21 16:41:01 +0100

    logger

--- lock_file_remove.py
+++ lock_file_remove.py
@@ -1,5 +1,8 @@
+import structlog
+
 from app.store.writer import remove_from_bucket
 from google.api_core.exceptions import NotFound
+logger = structlog.get_logger()
 
 BUCKET_NAME = "sdx-tester-lock"
 FILE_NAME = 'lock.txt'
@@ -9,7 +12,7 @@ def remove_lock_file():
     try:
         remove_from_bucket(FILE_NAME, BUCKET_NAME)
     except NotFound as err:
-        print(f'File does not exist: {err}')
+        logger.error(f'File does not exist: {err}')
         raise NotFound
 
 
