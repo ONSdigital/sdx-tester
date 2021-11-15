11a13052b055f5f95b48b42e3729601927ab43a1
Jon Kent <jonkent95@gmail.com>
2021-11-15 16:41:22 +0000

    Add new keys

--- app/jwt/encryption.py
+++ app/jwt/encryption.py
@@ -20,30 +20,46 @@ def encrypt_survey(submission: dict, eq_version_3: bool = False) -> str:
     Encryption is used to ensure only SDX can read a survey response. Signing is used to ensure SDX only trusts encrypted
     responses sent from eQ.
     """
-    key1 = open("test_sdx-public-jwt.yaml")
-    key2 = open("test_eq-private-signing.yaml")
+    sdx_key = open("keys/test_sdx-public-jwt.yaml")
+    eq_key = open("keys/test_eq-private-signing.yaml")
+    eqv3_key = open("keys/test_eqv3-private-signing.yaml")
 
-    key_store = load_keys(key1, key2)
+    if eq_version_3:
+        key_store = load_keys(sdx_key, eqv3_key)
+    else:
+        key_store = load_keys(sdx_key, eq_key)
+
     payload = encrypt(submission, key_store, 'submission')
-    key1.close()
-    key2.close()
+
+    sdx_key.close()
+    eq_key.close()
+    eqv3_key.close()
+
     return payload
 
 
-def decrypt_survey(payload: bytes) -> dict:
+def decrypt_survey(payload: bytes, eq_version_3: bool = False) -> dict:
     """
     Decrypts an encrypted bytes payload using sdx private key and verifies the signature using the signing public key
 
     The payload needs to be a JWE encrypted using SDX's public key.
     The JWE ciphertext should represent a JWS signed by EQ using their private key and with the survey json as the claims set.
     """
-    key1 = open("test_sdx-private-jwt.yaml")
-    key2 = open("test_eq-public-signing.yaml")
-    key_store = load_keys(key1, key2)
+    sdx_key = open("keys/test_sdx-private-jwt.yaml")
+    eq_key = open("keys/test_eq-public-signing.yaml")
+    eqv3_key = open("keys/test_eq-public-signing.yaml")
+
+    if eq_version_3:
+        key_store = load_keys(sdx_key, eqv3_key)
+    else:
+        key_store = load_keys(sdx_key, eq_key)
+
     b_payload = payload.decode('utf-8')
     decrypted_json = sdc_decrypt(b_payload, key_store, KEY_PURPOSE_SUBMISSION)
-    key1.close()
-    key2.close()
+    sdx_key.close()
+    eq_key.close()
+    eqv3_key.close()
+
     return decrypted_json
 
 
