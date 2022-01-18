444ea8bfd197004c0dc75fd8ba301c20e6660d52
Luke Joseph <ldjdev12@gmail.com>
2022-01-18 10:44:22 +0000

    removed comments

--- integration_tests/test_quarantine.py
+++ integration_tests/test_quarantine.py
@@ -63,7 +63,6 @@ class TestQuarantine(unittest.TestCase):
         self.assertIsNone(result.receipt, f'{key} should not produce a receipt!')
         print("PASSED")
 
-    # This one failed
     def test_missing_survey_id(self):
         self.survey.pop('survey_id')
         self.run_with_survey(self.survey)
@@ -76,7 +75,6 @@ class TestQuarantine(unittest.TestCase):
         self.survey.pop('data')
         self.run_with_survey(self.survey)
 
-    # This one also failed
     def test_missing_metadata(self):
         self.survey.pop('metadata')
         self.run_with_survey(self.survey)
